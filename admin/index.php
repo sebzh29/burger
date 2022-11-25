8c32a1680c8c0d358a7c80fa9c7c31107ba3f43e
unknown <sebzh29@outlook.com>
2022-11-25 18:59:53 +0100

    ajout fonction recup donnees dans db

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
         <link href='http://fonts.googleapis.com/css?family=Holtwood+One+SC' rel='stylesheet' type='text/css'>
-        <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"> -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 
         <link rel="stylesheet" href="../css/styles.css">
@@ -30,28 +30,32 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Item 1</td>
-                        <td>Description 1</td>
-                        <td>Prix 1</td>
-                        <td>Catégorie 1</td>
-                        <td width=300>
-                            <a class="btn btn-default" href="view.php?id=1"><span class="glyphicon glyphicon-eye-open"></span> Voir</a>
-                            <a class="btn btn-primary" href="update.php?id=1"><span class="glyphicon glyphicon-pencil"></span> Modifier</a>
-                            <a class="btn btn-danger" href="delete.php?id=1"><span class="glyphicon glyphicon-remove"></span> Supprimer</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Item 2</td>
-                        <td>Description 2</td>
-                        <td>Prix 2</td>
-                        <td>Catégorie 2</td>
-                        <td width=300>
-                            <a class="btn btn-default" href="view.php?id=2"><span class="glyphicon glyphicon-eye-open"></span> Voir</a>
-                            <a class="btn btn-primary" href="update.php?id=2"><span class="glyphicon glyphicon-pencil"></span> Modifier</a>
-                            <a class="btn btn-danger" href="delete.php?id=2"><span class="glyphicon glyphicon-remove"></span> Supprimer</a>
-                        </td>
-                    </tr>
+                    <?php
+                        require 'database.php';
+                        $db = Database::connect();
+
+                        $statement = $db->query('SELECT items.id, items.name, items.description, items.price, categories.name AS category
+                        FROM items LEFT JOIN categories ON items.category = categories.id
+                        ORDER BY items.id DESC');
+                        while($item = $statement->fetch())
+                        {
+                            echo '<tr>';
+                                echo '<td>' . $item['name'] . '</td>';
+                                echo '<td>' . $item['description'] . '</td>';
+                                echo '<td>' . $item['price'] . '</td>';
+                                echo '<td>' . $item['category'] . '</td>';
+                               
+                                echo '<td width=300>';
+                                echo '<a class="btn btn-default" href="view.php?id=' . $item['id'] . '"><span class="glyphicon glyphicon-eye-open"></span> Voir</a>';
+                                echo ' ';                            
+                                echo '<a class="btn btn-primary" href="update.php?id=' . $item['id'] . '"><span class="glyphicon glyphicon-pencil"></span> Modifier</a>';
+                                echo ' ';                    
+                                echo '<a class="btn btn-danger" href="delete.php?id=' . $item['id'] . '"><span class="glyphicon glyphicon-remove"></span> Supprimer</a>';
+                                echo '</td>';
+                            echo '</tr>';
+                        }
+                    ?>
+                    
                 </tbody>
             </table>
         </div>
